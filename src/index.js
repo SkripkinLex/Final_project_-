00a0e0aaff8a1856a59f4a3e0c655ea76bece253
Skripkin <skripkinlexxx@gmail.com>
2023-03-22 21:26:05 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './component/App/App';
 import { Provider } from 'react-redux';
 import { store } from './component/store';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 export const baseUrl = 'http://localhost:3333'
